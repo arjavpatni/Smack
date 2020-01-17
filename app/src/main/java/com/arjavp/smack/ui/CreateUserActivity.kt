790c9d2af98b24d219a39d2fe9f9230d7e7539f4
Arjav <arjavpatni@hotmail.com>
2020-01-17 16:40:56 +0530

    Create User activity and sign_up btn intent

---
+++ app/src/main/java/com/arjavp/smack/ui/CreateUserActivity.kt
@@ -0,0 +1,26 @@
+package com.arjavp.smack.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.arjavp.smack.R
+
+class CreateUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+
+    fun generateUserAvatar(view: View){
+
+    }
+
+    fun createUserClicked(view: View){
+
+    }
+
+    fun generateColorClicked(view: View){
+
+    }
+}
