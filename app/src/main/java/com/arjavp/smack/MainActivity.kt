14fb77f06097a21effa6ea5775ecef88a9fe9d13
Arjav <arjavpatni@hotmail.com>
2020-01-29 00:05:08 +0530

    getting channels from response and adapter for channels list view.

--- app/src/main/java/com/arjavp/smack/MainActivity.kt
+++ app/src/main/java/com/arjavp/smack/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.ArrayAdapter
 import android.widget.EditText
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AlertDialog
@@ -37,6 +38,13 @@ import kotlinx.android.synthetic.main.nav_header_main.*
 class MainActivity : AppCompatActivity() {
 
     val socket = IO.socket(SOCKET_URL)
+    lateinit var channelAdapter: ArrayAdapter<Channel>
+
+    //creating an adapter for channel ListView
+    private fun setupAdapters(){
+        channelAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, MessageService.channels)
+        channel_list.adapter = channelAdapter
+    }
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -66,6 +74,7 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+        setupAdapters()
 
     }
 
@@ -85,7 +94,7 @@ class MainActivity : AppCompatActivity() {
 
 
     private val userDataChangeReceiver = object : BroadcastReceiver() {
-        override fun onReceive(context: Context?, intent: Intent?) {
+        override fun onReceive(context: Context, intent: Intent?) {
             if (AuthService.isLoggedIn) {
                 userNameNavHeader.text = UserDataService.name
                 userEmailNavHeader.text = UserDataService.email
@@ -98,6 +107,12 @@ class MainActivity : AppCompatActivity() {
                     )
                 )
                 loginBtnNavHeader.text = "Logout"
+                MessageService.getChannels(context){ complete ->
+                    if(complete){
+                        //to notify the adapter that data set has changed and reload the listView
+                        channelAdapter.notifyDataSetChanged()
+                    }
+                }
             }
         }
     }
@@ -163,8 +178,8 @@ class MainActivity : AppCompatActivity() {
 
             val newChannel = Channel(channelName,channelDescription,channelId)
             MessageService.channels.add(newChannel)
+            channelAdapter.notifyDataSetChanged()
         }
-        println(args[0] as String)
     }
 
     fun sendMsgBtnClicked(view: View) {
--- app/src/main/java/com/arjavp/smack/Services/MessageService.kt
+++ app/src/main/java/com/arjavp/smack/Services/MessageService.kt
@@ -1,9 +1,57 @@
 package com.arjavp.smack.Services
+import android.content.Context
+import android.util.Log
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.Volley
 import com.arjavp.smack.Model.Channel
+import com.arjavp.smack.Utlities.URL_GET_CHANNELS
+import org.json.JSONArray
+import org.json.JSONException
 
 // to download and store the channels & messages
 object MessageService {
 
     val channels = ArrayList<Channel>()
+
+    fun getChannels(context: Context, complete: (Boolean) -> Unit){
+
+        val channelsRequest = object : JsonArrayRequest(Method.GET, URL_GET_CHANNELS, null, Response.Listener {response ->
+
+            try {
+                //looping through the arrayList of JSON objects.
+                for(x in 0 until response.length()){
+                    val channel = response.getJSONObject(x)
+                    val name = channel.getString("name")
+                    val chanDesc = channel.getString("description")
+                    val channelId = channel.getString("_id")
+
+                    val newChannel = Channel(name, chanDesc, channelId)
+                    //add this to array list of channels
+                    //"this" to access channels array list in this file.
+                    this.channels.add(newChannel)
+                }
+                complete(true)
+            }catch (e: JSONException){
+                Log.d("JSON","EXC: "+ e.localizedMessage)
+                complete(false)
+            }
+
+        }, Response.ErrorListener {
+            Log.d("ERROR","Could not retrieve channels")
+            complete(false)
+        }){
+            override fun getBodyContentType(): String {
+                return "application/json; charset=utf-8"
+            }
+
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers = HashMap<String,String>()
+                headers.put("Authorization", "Bearer ${AuthService.authToken}")
+                return headers
+            }
+        }
+        Volley.newRequestQueue(context).add(channelsRequest)
+    }
 
 }
