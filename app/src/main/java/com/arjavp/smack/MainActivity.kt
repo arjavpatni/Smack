b66daf5d74ef007afb34895e2a8ecc66c19e76f7
Arjav <arjavpatni@hotmail.com>
2020-01-30 15:31:33 +0530

    fun getMessages for message JsonArrayRequest, appended to messages arrayList, clearMessages and clearChannels funs for logout.

--- app/src/main/java/com/arjavp/smack/MainActivity.kt
+++ app/src/main/java/com/arjavp/smack/MainActivity.kt
@@ -134,9 +134,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun updateWithChannel(){
-        //changing text view in main activity from Please LogIn to channel name.
         mainChannelName.text="#${selectedChannel?.name}"
-        //download messages with channel.
+        //changing text view in main activity from Please LogIn to channel name.
+        if(selectedChannel != null){
+            MessageService.getMessages(selectedChannel!!.id){complete ->
+                if(complete){
+                }
+            }
+        }//download messages with channel.
     }
 
     override fun onBackPressed() {
@@ -188,31 +193,36 @@ class MainActivity : AppCompatActivity() {
         //args is array list of type "Any" so we have to cast is as String.
         //this listener its on a worker thread. We need to get back to main thread to update our UI
         //following brings us back to main thread, visit Threads documentation to know more.
-        runOnUiThread {
-            val channelName = args[0] as String
-            val channelDescription = args[1] as String
-            val channelId = args[2] as String
+        if(App.prefs.isLoggedIn){
+            runOnUiThread {
+                val channelName = args[0] as String
+                val channelDescription = args[1] as String
+                val channelId = args[2] as String
 
-            val newChannel = Channel(channelName,channelDescription,channelId)
-            MessageService.channels.add(newChannel)
-            channelAdapter.notifyDataSetChanged()
+                val newChannel = Channel(channelName,channelDescription,channelId)
+                MessageService.channels.add(newChannel)
+                channelAdapter.notifyDataSetChanged()
+            }
         }
     }
-
     //listener for messages. checkout io.emit(...) in index.js with event "messageCreated"
     //for the variable positions in args arrayList.
     private val onNewMessage = Emitter.Listener { args->
-        runOnUiThread{
-            val msgBody = args[0] as String
-            val channelId = args[2] as String
-            val userName = args[3] as String
-            val userAvatar = args[4] as String
-            val userAvatarColor = args[5] as String
-            val id = args[6] as String
-            val timeStamp = args[7] as String
+        if(App.prefs.isLoggedIn){
+            runOnUiThread{
+                val channelId = args[2] as String
+                if(channelId == selectedChannel?.id){
+                    val msgBody = args[0] as String
+                    val userName = args[3] as String
+                    val userAvatar = args[4] as String
+                    val userAvatarColor = args[5] as String
+                    val id = args[6] as String
+                    val timeStamp = args[7] as String
 
-            val newMessage = Message(msgBody, userName, channelId, userAvatar, userAvatarColor, id, timeStamp)
-            MessageService.messages.add(newMessage)
+                    val newMessage = Message(msgBody, userName, channelId, userAvatar, userAvatarColor, id, timeStamp)
+                    MessageService.messages.add(newMessage)
+                }
+            }
         }
     }
 
--- app/src/main/java/com/arjavp/smack/Services/MessageService.kt
+++ app/src/main/java/com/arjavp/smack/Services/MessageService.kt
@@ -1,14 +1,12 @@
 package com.arjavp.smack.Services
-import android.content.Context
 import android.util.Log
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonArrayRequest
-import com.android.volley.toolbox.Volley
 import com.arjavp.smack.Controller.App
 import com.arjavp.smack.Model.Channel
 import com.arjavp.smack.Model.Message
 import com.arjavp.smack.Utlities.URL_GET_CHANNELS
-import org.json.JSONArray
+import com.arjavp.smack.Utlities.URL_GET_MESSAGES
 import org.json.JSONException
 
 // to download and store the channels & messages
@@ -57,4 +55,51 @@ object MessageService {
         App.prefs.requestQueue.add(channelsRequest)
     }
 
+    fun getMessages(channelId: String, complete: (Boolean)-> Unit){
+
+        val url = "$URL_GET_MESSAGES$channelId"
+        val messagesRequest = object : JsonArrayRequest(Method.GET, URL_GET_MESSAGES, null, Response.Listener { response ->
+            clearMessages()//to make way for new messages (on switching to a new channel)
+            try{
+                for (x in 0 until response.length()){
+                    val message = response.getJSONObject(x)
+                    val messageBody =message.getString("messageBody") //get keys from index.js
+                    val channelId = message.getString("channelId")
+                    val id = message.getString("_id")
+                    val userName = message.getString("userName")
+                    val userAvatar = message.getString("userAvatar")
+                    val userAvatarColor = message.getString("userAvatarColor")
+                    val timeStamp = message.getString("timeStamp")
+
+                    val newMessage = Message(messageBody, userName, channelId, userAvatar, userAvatarColor, id, timeStamp)
+                    messages.add(newMessage)
+                }
+                complete(true)
+            }catch (e: JSONException){
+                Log.d("JSON","EXC: "+ e.localizedMessage)
+                complete(false)
+            }
+        },
+            Response.ErrorListener {
+                Log.d("ERROR","Could not retrieve channels")
+                complete(false)
+            }){
+            override fun getBodyContentType(): String {
+                return "application/json; charset=utf-8"
+            }
+
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers = HashMap<String,String>()
+                headers.put("Authorization", "Bearer ${App.prefs.authToken}")
+                return headers
+            }
+        }
+        App.prefs.requestQueue.add(messagesRequest)
+    }
+    fun clearMessages(){
+        messages.clear()
+    }
+    fun clearChannels(){
+        channels.clear()
+    }
 }
